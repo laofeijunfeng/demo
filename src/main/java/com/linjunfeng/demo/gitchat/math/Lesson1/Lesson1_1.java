681894f2b82071a0b1dd8dcc43f9b61fb8e9ef35
linjunfeng <laofeijunfeng@163.com>
2019-03-22 12:03:11 +0800

    练习：二进制与十进制间的互相转换

---
+++ src/main/java/com/linjunfeng/demo/gitchat/math/Lesson1/Lesson1_1.java
@@ -0,0 +1,36 @@
+package com.linjunfeng.demo.gitchat.math.Lesson1;
+
+import java.math.BigInteger;
+
+public class Lesson1_1 {
+
+    /**
+     * 十进制转二进制
+     * @param decimalSource
+     * @return
+     */
+    public static String decimalToBinary(int decimalSource) {
+        // 转换成 BigInteger 类型，默认是十进制
+        BigInteger bi = new BigInteger(String.valueOf(decimalSource));
+        // 参数 2 指定的是转化为二进制
+        return bi.toString(2);
+    }
+
+    /**
+     * 二进制转十进制
+     * @param binaryString
+     * @return
+     */
+    public static int binaryToDecimal(String binaryString) {
+        // 转换为 BigInteger 类型，参数 2 指的是二进制
+        BigInteger bi = new BigInteger(binaryString, 2);
+        return Integer.parseInt(bi.toString());
+    }
+
+    public static void main(String[] args) {
+        int a = 53;
+        String b = "110101";
+        System.out.println(String.format("数字 %d 的二进制是 %s", a, decimalToBinary(a)));
+        System.out.println(String.format("数字 %s 的十进制是 %d", b, binaryToDecimal(b)));
+    }
+}
