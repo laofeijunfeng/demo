73823dbed5d3dbeb7d11f87dbbb4faa65d48c6ee
linjunfeng <laofeijunfeng@163.com>
2019-03-26 14:06:53 +0800

    demo：测试添加thymeleaf模板引擎

---
+++ src/main/java/com/linjunfeng/demo/thymeleaf/IndexController.java
@@ -0,0 +1,14 @@
+package com.linjunfeng.demo.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(ModelMap map) {
+        map.addAttribute("message", "i am a thymeleaf test page");
+        return "index";
+    }
+}
