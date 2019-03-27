07e66e1c09c676a05da601cbd3aea3eb9f9217a4
linjunfeng <laofeijunfeng@163.com>
2019-03-27 14:55:15 +0800

    mqtt 配置提交

--- src/main/java/com/linjunfeng/demo/mqtt/config/MqttClientConfig.java
+++ src/main/java/com/linjunfeng/demo/mqtt/config/MqttClientConfig.java
@@ -1,15 +1,68 @@
 package com.linjunfeng.demo.mqtt.config;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MqttClientConfig {
+
     public static int qos = 1;
     private static String broker = "tcp://47.94.195.156:1883";
     private static String userName = "links2all";
     private static String password = "a5I2bsRsfP1kpgaQ";
     private static MqttConnectOptions connectOptions;
     private static MqttClient mqttClient;
+
+    MqttClientConfig() {
+        connectOptions = new MqttConnectOptions();
+        connectOptions.setCleanSession(true);
+        connectOptions.setUserName(userName);
+        connectOptions.setPassword(password.toCharArray());
+        connectOptions.setConnectionTimeout(0);
+        connectOptions.setKeepAliveInterval(20);
+        connectOptions.setAutomaticReconnect(true);
+    }
+
+    public static MqttClient connect(String clientId) throws MqttException {
+        MemoryPersistence persistence = new MemoryPersistence();
+        mqttClient = new MqttClient(broker, clientId, persistence);
+        mqttClient.setCallback(new PushCallback(mqttClient));
+        mqttClient.connect(connectOptions);
+        return mqttClient;
+    }
+
+    private static class PushCallback implements MqttCallback {
+
+        private MqttClient mqttClient;
+
+        PushCallback(MqttClient mqttClient) {
+            this.mqttClient = mqttClient;
+        }
+
+        @Override
+        public void connectionLost(Throwable throwable) {
+            // 连接丢失后，一般在这里面进行重连
+            System.out.println("掉线啦=========================" + mqttClient.getClientId());
+            try {
+                System.out.println("重连中=========================");
+                mqttClient.reconnect();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+            // subscribe后得到的消息会执行到这里面
+            System.out.println("接收消息主题 : " + topic);
+            System.out.println("接收消息Qos : " + mqttMessage.getQos());
+            System.out.println("接收消息内容 : " + new String(mqttMessage.getPayload()));
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+            System.out.println("deliveryComplete---------" + iMqttDeliveryToken.isComplete());
+        }
+    }
 }
--- src/main/java/com/linjunfeng/demo/thymeleaf/controller/IndexController.java
+++ src/main/java/com/linjunfeng/demo/thymeleaf/controller/IndexController.java
@@ -6,9 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class IndexController {
