2f65035471eddaeaf1b75033b2c44a184482323b
linjunfeng <laofeijunfeng@163.com>
2019-04-08 16:45:53 +0800

    currentThread()方法测试

---
+++ src/main/java/com/linjunfeng/demo/thread/Demo4/Demo4_1.java
@@ -0,0 +1,26 @@
+package com.linjunfeng.demo.thread.Demo4;
+
+public class Demo4_1 extends Thread {
+
+    public Demo4_1() {
+        System.out.println("Demo4_1 --- begin");
+        System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+        System.out.println("this.getName() = " + this.getName());
+        System.out.println("Demo4_1 --- end");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("run --- begin");
+        System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+        System.out.println("this.getName() = " + this.getName());
+        System.out.println("run --- end");
+    }
+
+    public static void main(String[] args) {
+        Demo4_1 demo4_1 = new Demo4_1();
+        Thread thread = new Thread(demo4_1);
+        thread.setName("A");
+        thread.start();
+    }
+}
