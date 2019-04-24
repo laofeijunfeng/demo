124cdf8826c6a395cd4bb9b716e4cace398aaac7
linjunfeng <laofeijunfeng@163.com>
2019-04-24 16:27:25 +0800

    验证线程变量的隔离性

---
+++ src/main/java/com/linjunfeng/demo/thread/communication/threadLocal/README.md
@@ -0,0 +1,3 @@
+### 类 ThreadLocal 的使用
+
+类 ThreadLocl 主要解决的是每个线程绑定自己的值。
---
+++ src/main/java/com/linjunfeng/demo/thread/communication/threadLocal/demo1/Tools.java
@@ -0,0 +1,5 @@
+package com.linjunfeng.demo.thread.communication.threadLocal.demo1;
+
+public class Tools {
+    public static ThreadLocal t1 = new ThreadLocal();
+}
