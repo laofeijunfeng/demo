deb0fc3114550564fe299e0d1e48775dad660d9c
linjunfeng <laofeijunfeng@163.com>
2019-06-05 17:07:14 +0800

    获取数据

---
+++ src/main/java/com/linjunfeng/demo/zookeeper/api/getData/GetDataApiSyncUsage.java
@@ -0,0 +1,39 @@
+package com.linjunfeng.demo.zookeeper.api.getData;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.concurrent.CountDownLatch;
+
+public class GetDataApiSyncUsage implements Watcher {
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static ZooKeeper zk = null;
+    private static Stat stat = new Stat();
+
+    public static void main(String[] args) throws Exception {
+        String path = "/zk-book2";
+        zk = new ZooKeeper("localhost:2181", 5000, new GetDataApiSyncUsage());
+        connectedSemaphore.await();
+        zk.create(path, "123".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println(new String(zk.getData(path, true, stat)));
+        System.out.println(stat.getCzxid() + ", " + stat.getMzxid() + ", " + stat.getVersion());
+        zk.setData(path, "123".getBytes(), -1);
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            if (Event.EventType.None == watchedEvent.getType() && null == watchedEvent.getPath())
+                connectedSemaphore.countDown();
+            else if (watchedEvent.getType() == Event.EventType.NodeDataChanged) {
+                try {
+                    System.out.println(new String(zk.getData(watchedEvent.getPath(), true, stat)));
+                    System.out.println(stat.getCzxid() + " , " + stat.getMzxid() + ", " + stat.getVersion());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
