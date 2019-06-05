38109033b7688ad519539f6f01424841cb9aa2a9
linjunfeng <laofeijunfeng@163.com>
2019-06-05 12:17:18 +0800

    异步创建节点

---
+++ src/main/java/com/linjunfeng/demo/zookeeper/script/createNode/ZookeeperCreateApiAsyncUsage.java
@@ -0,0 +1,38 @@
+package com.linjunfeng.demo.zookeeper.script.createNode;
+
+import org.apache.zookeeper.*;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ZookeeperCreateApiAsyncUsage implements Watcher {
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws Exception {
+        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 5000, new ZookeeperCreateApiAsyncUsage());
+        connectedSemaphore.await();
+
+        zooKeeper.create("/zk-test-ephemeral-", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL,
+                new IStringCallBack(), "I am context");
+
+        zooKeeper.create("/zk-test-ephemeral-", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL,
+                new IStringCallBack(), "I am context");
+
+        zooKeeper.create("/zk-test-ephemeral-", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL,
+                new IStringCallBack(), "I am context");
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+
+    static class IStringCallBack implements AsyncCallback.StringCallback {
+        @Override
+        public void processResult(int i, String s, Object o, String s1) {
+            System.out.println("Create path result: [" + i + ", " + s + ", " + o + ", real path name: " + s1);
+        }
+    }
+}
