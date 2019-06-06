710773e13ab0aefe6301b8614c3a0e57f2108779
linjunfeng <laofeijunfeng@163.com>
2019-06-06 11:14:53 +0800

    使用curator删除节点

---
+++ src/main/java/com/linjunfeng/demo/zookeeper/curator/deleteNode/DelDataSample.java
@@ -0,0 +1,27 @@
+package com.linjunfeng.demo.zookeeper.curator.deleteNode;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+
+public class DelDataSample {
+    static String path = "/zk-book/c1";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
+            .sessionTimeoutMs(5000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        client.create().creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath(path, "init".getBytes());
+        Stat stat = new Stat();
+        client.getData().storingStatIn(stat).forPath(path);
+        client.delete().deletingChildrenIfNeeded()
+                .withVersion(stat.getVersion()).forPath(path);
+    }
+}
