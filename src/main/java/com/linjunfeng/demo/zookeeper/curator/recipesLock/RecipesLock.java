9cfea32de8c8a9fee96e1098eb80aaf208dc785e
linjunfeng <laofeijunfeng@163.com>
2019-06-06 17:34:32 +0800

    使用curator实现分布式锁

---
+++ src/main/java/com/linjunfeng/demo/zookeeper/curator/recipesLock/RecipesLock.java
@@ -0,0 +1,42 @@
+package com.linjunfeng.demo.zookeeper.curator.recipesLock;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
+public class RecipesLock {
+    static String lock_path = "/curator_recipes_lock_path";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3)).build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        final InterProcessMutex lock = new InterProcessMutex(client, lock_path);
+        final CountDownLatch down = new CountDownLatch(1);
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                try {
+                    down.await();
+                    lock.acquire();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+                String orderNo = sdf.format(new Date());
+                System.out.println("生成的订单号是：" + orderNo);
+                try {
+                    lock.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+        down.countDown();
+    }
+}
---
+++ src/main/java/com/linjunfeng/demo/zookeeper/curator/recipesLock/RecipesNoLock.java
@@ -0,0 +1,22 @@
+package com.linjunfeng.demo.zookeeper.curator.recipesLock;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
+public class RecipesNoLock {
+    public static void main(String[] args) throws Exception {
+        final CountDownLatch down = new CountDownLatch(1);
+        for (int i = 0; i < 10; i++){
+            new Thread(() -> {
+                try {
+                    down.await();
+                } catch (Exception ignored) { }
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+                String orderNo = sdf.format(new Date());
+                System.out.println("生成的订单号是：" + orderNo);
+            }).start();
+        }
+        down.countDown();
+    }
+}
