1d9736a1cd1bfcb92586a1f2ad2f45eaf7d99f04
linjunfeng <laofeijunfeng@163.com>
2019-06-06 18:01:13 +0800

    工具ZKPaths的使用

---
+++ src/main/java/com/linjunfeng/demo/zookeeper/curator/ZKPaths/ZKPathsSample.java
@@ -0,0 +1,33 @@
+package com.linjunfeng.demo.zookeeper.curator.ZKPaths;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.ZKPaths;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZKPathsSample {
+    static String path = "/curator_zkpath_sample";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
+            .sessionTimeoutMs(5000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3)).build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        ZooKeeper zooKeeper = client.getZookeeperClient().getZooKeeper();
+        System.out.println(ZKPaths.fixForNamespace(path, "sub"));
+        System.out.println(ZKPaths.makePath(path, "sub"));
+        System.out.println(ZKPaths.getNodeFromPath("/curator_zkpath_sample/sub1"));
+        ZKPaths.PathAndNode pn = ZKPaths.getPathAndNode("/curator_zkpath_sample/sub1");
+        System.out.println(pn.getPath());
+        System.out.println(pn.getNode());
+
+        String dir1 = path + "/child1";
+        String dir2 = path + "/child2";
+        ZKPaths.mkdirs(zooKeeper, dir1);
+        ZKPaths.mkdirs(zooKeeper, dir2);
+        System.out.println(ZKPaths.getSortedChildren(zooKeeper, path));
+        ZKPaths.deleteChildren(client.getZookeeperClient().getZooKeeper(), path, true);
+    }
+}
