5415667a7cb776568efa0489505ad1ab42b77f0e
linjunfeng <laofeijunfeng@163.com>
2019-06-06 11:08:01 +0800

    使用curator创建节点

---
+++ src/main/java/com/linjunfeng/demo/zookeeper/curator/createNode/CreateNodeSample.java
@@ -0,0 +1,22 @@
+package com.linjunfeng.demo.zookeeper.curator.createNode;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+public class CreateNodeSample {
+    static String path = "/zk-book/c1";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
+            .sessionTimeoutMs(5000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        client.create().creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath(path, "init".getBytes());
+    }
+}
