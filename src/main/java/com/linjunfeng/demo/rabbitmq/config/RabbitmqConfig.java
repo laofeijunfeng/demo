39ebe2fbd4eacfbcaae8e039c9d048f8e7902a7b
linjunfeng <laofeijunfeng@163.com>
2019-06-20 18:52:06 +0800

    增加 Rabbitmq 相关配置

---
+++ src/main/java/com/linjunfeng/demo/rabbitmq/config/RabbitmqConfig.java
@@ -0,0 +1,67 @@
+package com.linjunfeng.demo.rabbitmq.config;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Autowired
+    private CachingConnectionFactory connectionFactory;
+
+    @Autowired
+    private SimpleRabbitListenerContainerFactoryConfigurer factoryConfigurer;
+
+    /**
+     * 单一消费者
+     * @return
+     */
+    @Bean(name = "singleListenerContainer")
+    public SimpleRabbitListenerContainerFactory listenerContainer() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        factory.setConcurrentConsumers(1);
+        factory.setMaxConcurrentConsumers(1);
+        factory.setPrefetchCount(1);
+        factory.setTxSize(1);
+        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        return factory;
+    }
+
+    /**
+     * 多个消费者
+     * @return
+     */
+    @Bean(name = "multiListenerContainer")
+    public SimpleRabbitListenerContainerFactory multiListenerContainer() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factoryConfigurer.configure(factory,connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        factory.setAcknowledgeMode(AcknowledgeMode.NONE);
+        factory.setConcurrentConsumers(10);
+        factory.setMaxConcurrentConsumers(20);
+        factory.setPrefetchCount(5);
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        connectionFactory.setPublisherConfirms(true);
+        connectionFactory.setPublisherReturns(true);
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setConfirmCallback((correlationData, b, s) ->
+                System.out.println("消息发送成功：" + correlationData + " - " + b + " - " + s));
+        rabbitTemplate.setReturnCallback((message, i, s, s1, s2) ->
+                System.out.println("消息发送丢失：" + message + " - " + i + " - " + s + " - "  + s1 + " - "  + s2));
+        return rabbitTemplate;
+    }
+}
